aef1dd4d0387068a8538a2b8441fc262da887ec1
utk145 <utkarsh20241a0514@grietcollege.com>
2023-09-27 13:52:39 +0530

    pie-chart-done

--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 const Right = () => {
-  return (
-    <div className='right'>
-        
-    </div>
-  )
+    return (
+        <div className='right'>
+            <div className='right_1'><p>Results</p></div>
+            <div className='right_2'>
+                <p>Results</p>
+                <p>Score: 74%</p>
+            </div>
+            <div className="right_3">
+                <div className="pie_chart">
+
+                </div>
+                <p>
+                    Correct Answer <span style={{color:"#235784"}}></span><br/>
+                    Not Answered <span style={{color:"#bcdbdf"}}></span><br/>
+                    Wrong <span style={{color:"#40a8c4"}}></span>
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default Right
